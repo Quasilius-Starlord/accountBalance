ed3f7cbb155993e6379aa186dfbe8a169d25cee6
Quasilius Starlord <joysen833@gmail.com>
2024-08-29 21:14:59 +0530

    Prod endpoint

--- lib/utility/networkConfiguration/apiConfiguration.dart
+++ lib/utility/networkConfiguration/apiConfiguration.dart
@@ -1,6 +1,6 @@
 
 class ApiConfiguration{
-  static const String endPoint = "http://192.168.1.16:8080/";
+  static const String endPoint = "https://account-spring-app.onrender.com/users/login/";
   static const String login="users/login";
 }
 
